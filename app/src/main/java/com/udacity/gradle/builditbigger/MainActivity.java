d448af0eb5b03a3c7989f9efe97f307e403ab351
Serge Guzik <guzik.sergey@gmail.com>
2016-01-04 20:31:00 +0200

    Cancel joke fetching on screen rotate

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,6 +2,7 @@ package com.udacity.gradle.builditbigger;
 
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Pair;
@@ -9,14 +10,28 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressDialog mProgress;
+    private AsyncTask mRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mProgress != null) {
+            mProgress.dismiss();
+        }
+        if (mRequest != null) {
+            mRequest.cancel(true);
+        }
+    }
+
     public void tellJoke(View view){
-        ProgressDialog progress = ProgressDialog.show(this, getString(R.string.progress_dialog_title), getString(R.string.progress_dialog_message), true);
-        new JokeRequestAsyncTask().execute(new Pair<Context, ProgressDialog>(this, progress));
+        mProgress = ProgressDialog.show(this, getString(R.string.progress_dialog_title), getString(R.string.progress_dialog_message), true);
+        mRequest = new JokeRequestAsyncTask().execute(new Pair<Context, ProgressDialog>(this, mProgress));
     }
 }
